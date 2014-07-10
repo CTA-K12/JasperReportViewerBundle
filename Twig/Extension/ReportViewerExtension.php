21139cecbab9e6b5b80d30fdb09e1c3beb2faa69
lighthart <dlighthart@mesd.k12.or.us>
2014-07-10 14:48:53 -0700

    reportURI twig function added

--- Twig/Extension/ReportViewerExtension.php
+++ Twig/Extension/ReportViewerExtension.php
@@ -10,7 +10,7 @@ class ReportViewerExtension extends \Twig_Extension
 
 
     //InitRuntime function, called at runtime, overriding to get an instance of the twig environment
-    public function initRuntime(\Twig_Environment $environment) {
+    public function initRuntime( \Twig_Environment $environment ) {
         $this->environment = $environment;
     }
 
@@ -18,9 +18,10 @@ class ReportViewerExtension extends \Twig_Extension
     public function getFunctions() {
         //Function definition
         return array(
-            'mesd_jasper_reportviewer_stored_report_link' => new \Twig_Function_Method($this, 'renderStoredReportLink',  array('is_safe' => array('html'))),
-            'mesd_jasper_reportviewer_report_link' => new \Twig_Function_Method($this, 'renderReportLink',  array('is_safe' => array('html'))),
-            'mesd_jasper_reportviewer_home' => new \Twig_Function_Method($this, 'renderReportHome', array('is_safe' => array('html')))
+            'mesd_jasper_reportviewer_stored_report_link' => new \Twig_Function_Method( $this, 'renderStoredReportLink',  array( 'is_safe' => array( 'html' ) ) ),
+            'mesd_jasper_reportviewer_report_link' => new \Twig_Function_Method( $this, 'renderReportLink',  array( 'is_safe' => array( 'html' ) ) ),
+            'mesd_jasper_reportviewer_home' => new \Twig_Function_Method( $this, 'renderReportHome', array( 'is_safe' => array( 'html' ) ) ),
+            'mesd_jasper_reportviewer_uri' => new \Twig_Function_Method( $this, 'renderReportURI', array( 'is_safe' => array( 'html' ) ) ),
         );
     }
 
@@ -38,65 +39,83 @@ class ReportViewerExtension extends \Twig_Extension
     /**
      * Render a link to the report viewer home
      *
-     * @param  [type]  $linkText       [description]
-     * @param  string  $class          [description]
-     * @param  boolean $optionInNewTab [description]
+     * @param [type]  $linkText       [description]
+     * @param string  $class          [description]
+     * @param boolean $optionInNewTab [description]
      *
      * @return [type]                  [description]
      */
-    public function renderReportHome($linkText, $classes = ' ', $openInNewTab = true) {
+    public function renderReportURI() {
         return $this->environment->render(
-            'MesdJasperReportViewerBundle:Partials:reportHome.html.twig', 
-                array(
-                    'linkText' => $linkText,
-                    'classes' => $classes,
-                    'openInNewTab' => $openInNewTab));
+            'MesdJasperReportViewerBundle:Partials:reportURI.html.twig',
+            array(
+            )
+        );
+    }
+
+
+    /**
+     * Render a link to the report viewer home
+     *
+     * @param [type]  $linkText       [description]
+     * @param string  $class          [description]
+     * @param boolean $optionInNewTab [description]
+     *
+     * @return [type]                  [description]
+     */
+    public function renderReportHome( $linkText, $classes = ' ', $openInNewTab = true ) {
+        return $this->environment->render(
+            'MesdJasperReportViewerBundle:Partials:reportHome.html.twig',
+            array(
+                'linkText' => $linkText,
+                'classes' => $classes,
+                'openInNewTab' => $openInNewTab ) );
     }
 
     /**
      * Renders a link to a report
      *
-     * @param  string  $reportUri    The uri of the report
-     * @param  string  $linkText     The text to display for the link
-     * @param  string  $classes      The classes to have for the anchor tag
-     * @param  boolean $openInNewTab Whether to open in a new tab or not
-     * @param  boolean $hideHome     Whether to hide the home and history nav links
+     * @param string  $reportUri    The uri of the report
+     * @param string  $linkText     The text to display for the link
+     * @param string  $classes      The classes to have for the anchor tag
+     * @param boolean $openInNewTab Whether to open in a new tab or not
+     * @param boolean $hideHome     Whether to hide the home and history nav links
      *
      * @return string                The final link tag
      */
-    public function renderReportLink($reportUri, $linkText, $classes = ' ', $openInNewTab = true, $hideHome = true) {
+    public function renderReportLink( $reportUri, $linkText, $classes = ' ', $openInNewTab = true, $hideHome = true ) {
         return $this->environment->render(
-            'MesdJasperReportViewerBundle:Partials:reportLink.html.twig', 
-                array(
-                    'reportUri' => $reportUri,
-                    'linkText' => $linkText,
-                    'classes' => $classes,
-                    'openInNewTab' => $openInNewTab,
-                    'hideHome' => $hideHome));
+            'MesdJasperReportViewerBundle:Partials:reportLink.html.twig',
+            array(
+                'reportUri' => $reportUri,
+                'linkText' => $linkText,
+                'classes' => $classes,
+                'openInNewTab' => $openInNewTab,
+                'hideHome' => $hideHome ) );
     }
 
 
     /**
      * Renders a link to a stored report
      *
-     * @param  string  $reportUri    The uri of the report the request id is for
-     * @param  string  $requestId    The request id of the stored report
-     * @param  string  $linkText     The text to display for the link
-     * @param  string  $classes      The classes to have for the anchor tag
-     * @param  boolean $openInNewTab Whether to open in a new tab or not
-     * @param  boolean $hideHome     Whether to hide the home and history nav links
+     * @param string  $reportUri    The uri of the report the request id is for
+     * @param string  $requestId    The request id of the stored report
+     * @param string  $linkText     The text to display for the link
+     * @param string  $classes      The classes to have for the anchor tag
+     * @param boolean $openInNewTab Whether to open in a new tab or not
+     * @param boolean $hideHome     Whether to hide the home and history nav links
      *
      * @return string                The final link tag
      */
-    public function renderStoredReportLink($reportUri, $requestId, $linkText, $classes = ' ', $openInNewTab = true, $hideHome = true) {
+    public function renderStoredReportLink( $reportUri, $requestId, $linkText, $classes = ' ', $openInNewTab = true, $hideHome = true ) {
         return $this->environment->render(
-            'MesdJasperReportViewerBundle:Partials:storedReportLink.html.twig', 
+            'MesdJasperReportViewerBundle:Partials:storedReportLink.html.twig',
             array(
                 'reportUri' => $reportUri,
                 'requestId' => $requestId,
                 'linkText' => $linkText,
                 'classes' => $classes,
                 'openInNewTab' => $openInNewTab,
-                'hideHome' => $hideHome));
+                'hideHome' => $hideHome ) );
     }
 }
